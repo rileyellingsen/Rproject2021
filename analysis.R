d6aea36d28bf220cddaaa3e115eb65b637b8ec8a
Riley Ellingsen <rileyellingsen@gmail.com>
2021-12-02 21:40:02 -0500

    Created files and outlined code

---
+++ analysis.R
@@ -0,0 +1,11 @@
+#This script will use the source() function to load the functions defined in
+#supportingFunctions.R, compile all data into a single .csv file, process the
+#data included in the entire data set in order to answer the two questions
+#and provide graphical evidence for the answers contained. 
+
+
+#Question 1 - In which country (X or Y) did the disease outbreak likely begin?
+
+
+#Question 2 - If Country Y develops a vaccine for the disease, is it likely to
+#work for citizens of Country X?
---
+++ supportingFunctions.R
@@ -0,0 +1,24 @@
+#This script contains a number of functions that will accomplish various data 
+#handing or summary tasks.
+
+#Function 1 - Country X and Y have different traditions for the delimiter in 
+#their data files. Write a function that converts all files in a directory with 
+#space- or tab-delimited data (.txt) into comma-separated value files.
+
+
+
+#Function 2 - Write a function to compile data from all .csv files in a 
+#directory into a single .csv file. The compiled data should have the original 
+#twelve columns from daily data sheets, but also country and dayofYear columns. 
+#The user should be able to choose whether they want to remove rows with NA’s in
+#any columns, include NAs in the compiled data but be warned of their presence, 
+#or include NAs in the compiled data without a warning
+
+
+
+#Function 3 - Write a function to summarize the compiled data set in terms of 
+#number of screens run, percent of patients screened that were infected, male 
+#vs. female patients, and the age distribution of patients.
+
+
+
